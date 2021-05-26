54ef1671a08f70d510e48ae4927ef3ccd2b97832
Karina Kinzhigalieva <ualesh9797@gmail.com>
2021-05-26 22:58:27 +0600

    branch04 upgrade

--- lesson04/tests/acceptance/ModalCheckerCest.php
+++ lesson04/tests/acceptance/ModalCheckerCest.php
@@ -7,12 +7,12 @@ class ModalCheckerCest
     public function tryToTest(AcceptanceTester $I)
     {
         $I->amOnPage("");
-        $I->see("Blouse");
+        $I->waitForText("Blouse");
         $I->moveMouseOver("#homefeatured > li:nth-child(2)");
         $I->waitForElement("#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view");
         $I->click("#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view");
         $I->waitForElement("#index > div.fancybox-overlay.fancybox-overlay-fixed > div");
         $I->switchToFrame("/html/body/div[2]/div/div/div/div/iframe");
-        $I->see("Blouse");
+        $I->waitForText("Blouse", 10, "#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1");
     }
 }
